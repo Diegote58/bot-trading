c0436f781ce160783e2155cdaf0a5e760450246c
Diego Gimenez <diegote58@gmail.com>
2020-01-13 22:31:22 -0300

    Update ConstantConfig.java

--- src/main/java/com/io/trading/config/ConstantConfig.java
+++ src/main/java/com/io/trading/config/ConstantConfig.java
@@ -6,8 +6,8 @@ public interface ConstantConfig {
 
     public static final String BASE_URL = "https://api.invertironline.com";
 
-    public static final String USERNAME = "Diegote58";
-    public static final String PASSWORD = "Motocicleta123";
+    public static final String USERNAME = "user";
+    public static final String PASSWORD = "password";
     public static final String GRANT_TYPE_PASSWORD = "password";
     public static final String GRANT_TYPE_REFRESH_TOKEN = "refresh_token";
 
